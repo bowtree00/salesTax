bf0ee4ef38f050e359bcb9ba8ae651dde531e517
Christian Beaudrie <bowtree00@gmail.com>
2016-10-05 21:22:23 +0000

    removed comments

--- salesTax.js
+++ salesTax.js
@@ -22,6 +22,7 @@ var companySalesData = [
   }
 ];
 
+
 function sumSales(salesData) {
   var salesSum = 0;
 
@@ -35,58 +36,36 @@ function sumSales(salesData) {
 
 function calculateSalesTax(salesData, taxRates) {
 
-// pick the value from 'province' in salesData, then grab the value from taxRates[value]
-
 var salesProvince = salesData.province;
 var tax = taxRates[salesProvince];
-
 var salesTax = sumSales(salesData) * tax;
-//var salesTax = salesData.sales[0] * tax;
 
 return salesTax
-
-
 }
 
 
 
 function salesTaxReport(companySalesData, salesTaxRates) {
 var taxReport = {};
-  // console.log("companySalesData ", companySalesData)
 
   for (salesInstance of companySalesData) {
-
-    if (!taxReport[salesInstance.name]) {
+    if (!taxReport[salesInstance.name]) {  
       
       var salesSum = 0;
-
-      // for (i = 0; i < salesInstance.sales.length; i++) {
-      //   salesSum += Number(salesInstance.sales[i]);
-      // }
 
       taxReport[salesInstance.name] = {
         totalSales: sumSales(salesInstance),
         totalTaxes: calculateSalesTax(salesInstance, salesTaxRates)
       }
-
-
     } else {
-      
       tempObj = taxReport[salesInstance.name];
       tempObj.totalSales += sumSales(salesInstance);
       tempObj.totalTaxes += calculateSalesTax(salesInstance, salesTaxRates);
-
-      // taxReport[salesInstance.name].totalSales += Number(salesInstance.sales[0]);
-      // taxReport[salesInstance.name].totalTaxes += Number(salesInstance.sales[1]);
     }
  
   }
-  
 console.log("taxReport ", taxReport)
-
 }
-
-
 
 
 var results = salesTaxReport(companySalesData, salesTaxRates);
